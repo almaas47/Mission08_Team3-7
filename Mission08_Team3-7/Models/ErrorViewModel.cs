1a9c3bd0a84645bec57bc740f99cba86c339cd90
almaas47 <matt47@byu.edu>
2025-02-28 11:56:30 -0700

    Initial commit

---
+++ Mission08_Team3-7/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Mission08_Team3_7.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
